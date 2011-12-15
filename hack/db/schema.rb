683ae95883c979d708a34f31a4520f779ad0a045
grit <gritty@sympatico.ca>
2011-12-15 06:30:18 -0500

    added admin to users table

--- hack/db/schema.rb
+++ hack/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111214222428) do
+ActiveRecord::Schema.define(:version => 20111215112900) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(:version => 20111214222428) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "handle"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
